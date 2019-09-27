c36ea5c01190f9be7e9e71758c5c9aede45173b9
Lindsay Lopez <lopez95@berkeley.edu>
2019-09-26 21:14:44 -0700

    Study log for 9/26/2019

--- studylog.js
+++ studylog.js
@@ -1,7 +1,11 @@
 /* 
-09/24/2019
-Basic JavaScript: Local Scope and Functions - Basic JavaScript: Comparison with the Strict Inequality Operator
+09/26/2019
+- Working on Resume
+- Basic JavaScript: Selecting from Many Options with Switch Statements - Basic JavaScript: Testing Objects for Properties
 
 09/25/2019
-Basic JavaScript: Comparison with the Greater Than Operator - Basic JavaScript: Golf Code
+- Basic JavaScript: Comparison with the Greater Than Operator - Basic JavaScript: Golf Code
+
+09/24/2019
+- Basic JavaScript: Local Scope and Functions - Basic JavaScript: Comparison with the Strict Inequality Operator
 */
