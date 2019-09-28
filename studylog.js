a748300e149f3a02c3baf624dc03b643d232603e
Lindsay Lopez <lopez95@berkeley.edu>
2019-09-28 12:35:10 -0700

    Study log for 9/27 and 9/28/2019

--- studylog.js
+++ studylog.js
@@ -1,4 +1,10 @@
 /* 
+09/28/2019
+- Working on finishing touches for Cover Letter and Resume.
+
+09/27/2019
+- Working on Cover Letter and Resume
+
 09/26/2019
 - Working on Resume
 - Basic JavaScript: Selecting from Many Options with Switch Statements - Basic JavaScript: Testing Objects for Properties
