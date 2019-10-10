e1e0d8ee164180415c8d44da64883f8b89287d7b
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-09 19:51:54 -0700

    completed findElement

--- introduction-to-basic-algorithm-scripting/algorithms.js
+++ introduction-to-basic-algorithm-scripting/algorithms.js
@@ -110,3 +110,13 @@ function truncateString(str, num) {
   }
   return str;
 }
+
+/*
+Finders Keepers
+
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+*/
+function findElement(arr, func) {
+  let elementsThatPass = arr.filter(element => func(element));
+  return elementsThatPass.length > 0 ? elementsThatPass[0] : undefined;
+}
