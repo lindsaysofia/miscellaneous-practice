1f4f9e61f3ded8dd2a799329bd89adc43d533072
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-09 20:08:20 -0700

    completed titleCase

--- introduction-to-basic-algorithm-scripting/algorithms.js
+++ introduction-to-basic-algorithm-scripting/algorithms.js
@@ -132,3 +132,16 @@ function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
   return bool === true || bool === false;
 }
+
+/*
+Title Case a Sentence
+
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+*/
+function titleCase(str) {
+  return str.split(' ').map(word => {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  }).join(' ');
+}
