666c4e169a1514dd62999469fbab15cdac45eadf
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-09 20:49:54 -0700

    completed mutation

--- introduction-to-basic-algorithm-scripting/algorithms.js
+++ introduction-to-basic-algorithm-scripting/algorithms.js
@@ -198,3 +198,25 @@ function getIndexToIns(arr, num) {
   }
   return arrindex;
 }
+
+/*
+Mutations
+
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"]should return false because the string "hello" does not contain a "y".
+
+Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+*/
+function mutation(arr) {
+  let firstStr = arr[0].toLowerCase();
+  let secondStr = arr[1].toLowerCase();
+  for (let secondStrIndex = 0; secondStrIndex < secondStr.length; secondStrIndex++) {
+    if (!firstStr.includes(secondStr[secondStrIndex])) {
+      return false;
+    }
+  }
+  return true;
+}
