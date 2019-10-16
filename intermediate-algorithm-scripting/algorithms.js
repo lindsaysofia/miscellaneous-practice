90ee2805bb394370f7d5c78e499e47e15f713eb4
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-15 21:07:37 -0700

    completed whatIsInAName

--- intermediate-algorithm-scripting/algorithms.js
+++ intermediate-algorithm-scripting/algorithms.js
@@ -41,3 +41,47 @@ function diffArray(arr1, arr2) {
   });
   return newArr;
 }
+
+/*
+Seek and Destroy
+
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+Note
+You have to use the argumentsobject.
+
+Keep in mind next time: instead of setting arguments to an array (args), can just use spread operator i.e. funtion destroyer(arr, ...args)
+*/
+function destroyer(arr) {
+  // Remove all the values
+  let args = Array.from(arguments).splice(1);
+  return arr.filter(element => !args.includes(element));
+}
+
+/*
+Wherefore art thou
+
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+Keep in mind next time: Use filter/Every or Map!
+*/
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  collection.forEach(collectionObj => {
+    let add = true;
+    for (let property in source) {
+      if (!(collectionObj.hasOwnProperty(property) && collectionObj[property] === source[property])) {
+        add = false;
+      }
+    }
+    if (add) {
+      arr.push(collectionObj);
+    }
+  });
+  // Only change code above this line
+  return arr;
+}
