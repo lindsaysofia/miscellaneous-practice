2ab96107d4c908e32f8c001a1c87c1c0e312b6db
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-21 19:26:10 -0700

    completed uniteUnique

--- intermediate-algorithm-scripting/algorithms.js
+++ intermediate-algorithm-scripting/algorithms.js
@@ -183,4 +183,26 @@ function fearNotLetter(str) {
     sum -= str.charCodeAt(strIndex);
   }
   return (sum === 0) ? undefined : String.fromCharCode(sum);
+} 
+
+/*
+Sorted Union
+
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4]
+
+Keep in mind next time: use set?
+*/
+function uniteUnique(...args) {
+  let firstArr = args[0].slice();
+  for (let argsIndex = 1; argsIndex < args.length; argsIndex++) {
+     let nextArr = args[argsIndex].filter(element => !firstArr.includes(element));
+     firstArr = firstArr.concat(nextArr);
+  }
+  return firstArr;
 }
