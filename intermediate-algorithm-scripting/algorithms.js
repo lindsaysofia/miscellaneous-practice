9fc81e9c2c1b5d2c9c90750003f5c67f1edde0a6
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-21 20:55:22 -0700

    completed sumPrimes

--- intermediate-algorithm-scripting/algorithms.js
+++ intermediate-algorithm-scripting/algorithms.js
@@ -258,3 +258,33 @@ function sumFibs(num) {
   return sum;
 }
 
+/*
+Sum All Primes
+
+Sum all the prime numbers up to and including the provided number.
+
+A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+The provided number may not be a prime.
+*/
+function sumPrimes(num) {
+  let sum = 0;
+  while (num > 1) {
+    if (isPrime(num)) {
+      sum += num;
+    }
+    num--;
+  }
+  return sum;
+}
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
