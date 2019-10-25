9119b23e68dc289b85b0419344612d434a3b82c2
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-25 16:05:24 -0700

    completed addTogether

--- intermediate-algorithm-scripting/algorithms.js
+++ intermediate-algorithm-scripting/algorithms.js
@@ -437,3 +437,32 @@ function truthCheck(collection, pre) {
   return collection.every(object => object[pre]);
 }
 
+/*
+Arguments Optional
+
+Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+Calling this returned function with a single argument will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+
+addTogether(2, 3) should return 5.
+addTogether(2)(3) should return 5.
+addTogether(2)([3]) should return undefined.
+*/
+function addTogether(...args) {
+  if (typeof(args[0]) === 'number') {
+    if (args.length === 2) {
+      return (typeof(args[1]) === 'number') ? args[0] + args[1] : undefined;
+    }
+    return num => (typeof(num) === 'number') ? args[0] + num : undefined;
+  }
+  return undefined;
+}
+
